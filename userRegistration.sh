ea68c15fa32bf811a661b9bd4a10e8ec8e9f2400
tusharkhandejod <tusharkhandejod@gmail.com>
2020-11-05 17:30:35 +0530

    Code for validating Email ID is added

--- userRegistration.sh
+++ userRegistration.sh
@@ -23,5 +23,17 @@ else
 fi
 }
 
+function emailPattern(){
+read -p "Enter email id : " emailid
+pat="^(([A-Za-z0-9]+((\.|\-|\_|\+)?[A-Za-z0-9]?)*[A-Za-z0-9]+)|[A-Za-z0-9]+)@(([A-Za-z0-9]+)+((\.|\-|\_)?([A-Za-z0-9]+)+)*)+\.([A-Za-z]{2,})+$" 
+#all conditions true
+if [[ $emailid =~ $pat ]]
+then
+	echo "Valid Email Address"
+else
+	echo "Invalid Email Address"
+fi
+}
 validFirstName
 validLastName
+emailPattern
