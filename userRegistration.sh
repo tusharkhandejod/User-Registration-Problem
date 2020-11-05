11170d358e55be909c42db0ec4db5f52d4e8f482
tusharkhandejod <tusharkhandejod@gmail.com>
2020-11-05 17:25:52 +0530

    Code for validating Last Name is added

--- userRegistration.sh
+++ userRegistration.sh
@@ -11,4 +11,17 @@ else
 echo "Invalid First Name"
 fi
 }
+
+function validLastName() {
+read -p "Enter the last name:" lastName
+patternForLastName="^[[:upper:]]{1}[[:lower:]]{2,}$"
+if [[ $lastName =~ $patternForLastName ]]
+then
+        echo "Valid Last Name"
+else
+        echo "Invalid Last Name"
+fi
+}
+
 validFirstName
+validLastName
